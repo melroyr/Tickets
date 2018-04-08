6b554d00d81e4bd4d48d7f6993358fdebcfb1515
melroyr <melroyr@users.noreply.github.com>
2018-04-08 09:48:24 -0400

    added comments

--- src/main/java/com/dasanjos/tickets/service/TicketingService.java
+++ src/main/java/com/dasanjos/tickets/service/TicketingService.java
@@ -1,34 +1,32 @@
 package com.dasanjos.tickets.service;
 
-import java.util.List;
-
-import com.dasanjos.tickets.domain.Seat;
-
+/**
+ * Interface for the TicketingService implementation
+ * 
+ * @author melro
+ *
+ */
 public interface TicketingService {
 	/**
 	* The number of seats in the venue that are neither held nor reserved
 	*
-	* @return the number of tickets available in the venue
 	*/
 	void displaySeatsAvailable();
 	
 	/**
-	* Find and hold the best available seats for a customer
+	* Find and hold the best available seats for a customer as asked by the customer
 	*
-	* @param numSeats the number of seats to find and hold
+	* @param seats the seats the customer ask to hold
 	* @param customerEmail unique identifier for the customer
-	* @return a List of seats object identifying the specific seats and related
-	information
+	* @return transaction id identifying the specific seats held information
 	*/
 	long findAndHoldSeats(String seats, String customerEmail);
 	
 	/**
 	* Commit seats held for a specific customer
 	*
-	* @param seatHoldId the seat hold identifier
-	* @param customerEmail the email address of the customer to which the
-	seat hold is assigned
-	* @return a reservation confirmation code
+	* @param time the seat hold identifier
+	* @return success or failure to reserve
 	*/
 	int reserveSeats(long time);
 	}
