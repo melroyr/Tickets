517d45c640ca35eb158b92ccbb0366ad1c0f67e1
melroyr <melroyr@users.noreply.github.com>
2018-04-08 09:47:18 -0400

    added comments

--- src/main/java/com/dasanjos/tickets/app/Tickets.java
+++ src/main/java/com/dasanjos/tickets/app/Tickets.java
@@ -3,6 +3,13 @@ package com.dasanjos.tickets.app;
 import com.dasanjos.tickets.service.TicketingService;
 import com.dasanjos.tickets.service.TicketingServiceImpl;
 
+/**
+ * This is the driver class that starting the ticketing application and assist ticket issuers to issue tickets to ticket holders
+ * at high demand venues
+ * 
+ * @author melro
+ *
+ */
 public class Tickets {
 	
 	public static void main(String[] args) {
@@ -15,11 +22,10 @@ public class Tickets {
 			System.out.print("Enter Customer Email: ");
 			customerEmail = System.console().readLine();
 			System.out.println("customerEmail: " + customerEmail);
-			System.out.print("Enter seats required in the following format 01,02,03,04:");
+			System.out.print("Enter seats required in the following format 01,02,03,04: ");
 			seats = System.console().readLine();
-			System.out.println("seats: " + seats);
 			long time = ticketingService.findAndHoldSeats(seats, customerEmail);
-			System.out.println("Long time: " + time);
+			ticketingService.displaySeatsAvailable();
 			System.out.print("Your seats are held for 30 seconds. Please enter any key to confirm:");
 			seats = System.console().readLine();
 			System.out.println("confirm: " + seats);
@@ -29,7 +35,7 @@ public class Tickets {
 			} else if (resp == 0) {
 				System.out.println("Sorry, the hold timed out, please try again");
 			}
-			
+			ticketingService.displaySeatsAvailable();
 			System.out.print("Enter exit to exit, any other key to continue:");
 			action = System.console().readLine();
 			System.out.println("action: " + action);
